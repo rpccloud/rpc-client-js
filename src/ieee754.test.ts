6b9d743e8219bcf85205d6e045d4a924035af7eb
tslearn <tslearn@163.com>
2021-02-06 03:12:22 +0800

    fast dev

--- src/ieee754.test.ts
+++ src/ieee754.test.ts
@@ -1,4 +1,4 @@
-import { Ieee754 } from "./ieee754"
+import {Ieee754} from "./ieee754"
 
 describe("ieee754 tests", () => {
     test("Ieee754_read_write", () => {
@@ -18,9 +18,9 @@ describe("ieee754 tests", () => {
         }
         // boundary numbers
         for (const v of [
-            NaN, Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY, Number.EPSILON,
-            0, Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER, Number.MAX_VALUE,
-            Number.MIN_VALUE, Infinity,
+            NaN, Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY,
+            Number.EPSILON, 0, Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER,
+            Number.MAX_VALUE, Number.MIN_VALUE, Infinity,
         ]) {
             Ieee754.write(data, v, 0, true, 52, 8)
             expect(Ieee754.read(data, 0, true, 52, 8))
